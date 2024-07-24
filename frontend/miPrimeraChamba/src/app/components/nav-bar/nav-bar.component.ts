8006f3eb1462a235ccdb93094f1c58e197dd8567
dev1d123 <dhuamanio@unsa.edu.pe>
2024-07-24 17:29:15 -0500

    Redirecciones

--- frontend/miPrimeraChamba/src/app/components/nav-bar/nav-bar.component.ts
+++ frontend/miPrimeraChamba/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
@@ -19,7 +18,16 @@ export class NavBarComponent {
     this.registered = false;
     // authService.logout();
   }
+
   isRegistered(): boolean {
     return this.registered;
+  }
+
+  register() {
+    window.location.href = "/registrarse";
+  }
+  
+  login() {
+    window.location.href = "/iniciar-sesion";
   }
 }
