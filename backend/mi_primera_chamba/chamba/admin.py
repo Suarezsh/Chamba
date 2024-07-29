98f9a6e4b29616a16441da3865867dc9a8d25184
msuarez <87085119+Suarezsh@users.noreply.github.com>
2024-07-29 06:53:50 -0500

    Delete backend/mi_primera_chamba directory

--- backend/mi_primera_chamba/chamba/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Usuario, Empleador, Trabajador, Chamba, Mensaje
-
-admin.site.register(Usuario)
-admin.site.register(Empleador)
-admin.site.register(Trabajador)
-admin.site.register(Chamba)
-admin.site.register(Mensaje)
